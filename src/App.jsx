362ccc4c1544391fe853b6bee57a983a99bb7aeb
coffmanjrp <coffmanjrp@gmail.com>
2021-08-12 12:48:20 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="container">
+        <header className="header">
+          <h4 className="title">Live User Filter</h4>
+          <small className="subtitle">Search by name and/or location</small>
+          <input type="text" placeholder="Search" />
+        </header>
+        <ul className="user-list">
+          <li>
+            <h3>Loading...</h3>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
