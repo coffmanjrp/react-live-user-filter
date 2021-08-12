2683b652818b8e3e93e745e803fd2eaee45acfb6
coffmanjrp <coffmanjrp@gmail.com>
2021-08-12 13:03:24 -0700

    Fetch and display users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    try {
+      const count = 50;
+      const res = await fetch(`https://randomuser.me/api?results=${count}`);
+      const data = await res.json();
+
+      setUsers(data.results);
+    } catch (err) {
+      console.error(err);
+      console.log('Something is not good');
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
@@ -10,9 +30,25 @@ function App() {
           <input type="text" placeholder="Search" />
         </header>
         <ul className="user-list">
-          <li>
-            <h3>Loading...</h3>
-          </li>
+          {!users ? (
+            <li>
+              <h3>Loading...</h3>
+            </li>
+          ) : (
+            users.map((user) => (
+              <li key={user.login.uuid}>
+                <img src={user.picture.large} alt={user.name.first} />
+                <div className="user-info">
+                  <h4>
+                    {user.name.first} {user.name.last}
+                  </h4>
+                  <p>
+                    {user.location.city}, {user.location.country}
+                  </p>
+                </div>
+              </li>
+            ))
+          )}
         </ul>
       </div>
     </div>
