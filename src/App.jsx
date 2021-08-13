112406221f27ee29de7b3ae36b99a8791e100922
coffmanjrp <coffmanjrp@gmail.com>
2021-08-12 19:35:49 -0700

    Implement filtering users

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.scss';
 
 function App() {
   const [users, setUsers] = useState(null);
+  const [searchUser, setSearchUser] = useState(null);
 
   useEffect(() => {
     fetchUsers();
@@ -21,20 +22,46 @@ function App() {
     }
   };
 
+  const handleChange = (e) => {
+    const searchData = users.filter((user) => {
+      const firstName = user.name.first.toLocaleLowerCase();
+      const lastName = user.name.last.toLocaleLowerCase();
+      const regex = new RegExp(`${e.target.value}`, 'gi');
+
+      return firstName.match(regex) || lastName.match(regex);
+    });
+
+    setSearchUser(searchData);
+  };
+
   return (
     <div className="App">
       <div className="container">
         <header className="header">
           <h4 className="title">Live User Filter</h4>
           <small className="subtitle">Search by name and/or location</small>
-          <input type="text" placeholder="Search" />
+          <input
+            type="text"
+            onChange={(e) => handleChange(e)}
+            placeholder="Search"
+          />
         </header>
         <ul className="user-list">
-          {!users ? (
-            <li>
-              <h3>Loading...</h3>
-            </li>
-          ) : (
+          {searchUser ? (
+            searchUser.map((user) => (
+              <li key={user.login.uuid}>
+                <img src={user.picture.large} alt={user.name.first} />
+                <div className="user-info">
+                  <h4>
+                    {user.name.first} {user.name.last}
+                  </h4>
+                  <p>
+                    {user.location.city}, {user.location.country}
+                  </p>
+                </div>
+              </li>
+            ))
+          ) : users ? (
             users.map((user) => (
               <li key={user.login.uuid}>
                 <img src={user.picture.large} alt={user.name.first} />
@@ -48,6 +75,10 @@ function App() {
                 </div>
               </li>
             ))
+          ) : (
+            <li>
+              <h3>Loading...</h3>
+            </li>
           )}
         </ul>
       </div>
